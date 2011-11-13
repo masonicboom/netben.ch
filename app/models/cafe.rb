eb3dfd68c43133d0e194fcb97f9d0e1bd0d78642
Mason Simon <masonsimon@gmail.com>
2011-11-12 18:36:44 -0800

    reimplemented the ranking function in ruby since it was broken in postgresql previously

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -2,7 +2,10 @@ class Cafe < ActiveRecord::Base
   has_many :test_results
 
   def Cafe::bandwidth_ranked
-    Cafe.joins(:test_results).find(:all, :order => 'upload_mbps * download_mbps DESC', :group => 'cafe_id')
+    Cafe.includes(:test_results).all.sort_by do |cafe|
+      tr = cafe.test_results.find(:first, :order => 'upload_mbps * download_mbps DESC')
+      tr.upload_mbps * tr.download_mbps
+    end.reverse
   end
   
 end
